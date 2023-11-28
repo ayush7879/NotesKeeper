de9773d286c8b0a14d52559a111fa5f7a6f4ff2a
ayush7879 <112853530+ayush7879@users.noreply.github.com>
2023-11-28 20:29:38 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,16 @@
 const mongoose=require('mongoose');
-const mongoURI="mongodb://127.0.0.1:27017"
+
+// const mongoURI="mongodb://127.0.0.1:27017"
+const mongoURI="mongodb+srv://ayushagrawal:ayushagrawal@cluster0.5pnzafq.mongodb.net/?retryWrites=true&w=majority"
 mongoose.set('strictQuery', true)
 const  connectToMongo = async()=>{
-   mongoose.connect(mongoURI,()=>{
+//    mongoose.connect(mongoURI,()=>{
     
-        console.log("connect to mongo Successfully");
-    })
+//         console.log("connect to mongo Successfully");
+//     })
+mongoose.connect(mongoURI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }).then(() => console.log('DB Connected'));
 }
 module.exports = connectToMongo;
